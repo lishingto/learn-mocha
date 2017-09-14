8360c323683db79e6f38d3dfc1e62e1d66918414
Li Shing To <lishingto@gmail.com>
2017-09-14 15:56:41 +0800

    Added a test case for empty output

--- test/test.js
+++ test/test.js
@@ -20,6 +20,10 @@ describe('Cash Register', function(){
         assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
     });
 
+    it('getChange(999,999) should equal []', function(){
+        assert.deepEqual(C.getChange(999,999), [ ]);
+    });
+
     // try to force an error:
     it('getChange("random","str") to throw error', function(){
         assert.throws(function(){C.getChange("random","str")}, Error, "totalPayable and cashPaid MUST both be numbers");
