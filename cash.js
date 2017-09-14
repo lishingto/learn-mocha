066043484a0ba49fbe6ba710b88547eb9fb92646
Li Shing To <lishingto@gmail.com>
2017-09-14 16:06:49 +0800

    Conflict test

--- cash.js
+++ cash.js
@@ -2,6 +2,7 @@
 
 const C = {
   coins : [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1],
+
   /**
    * getChange returns and Array containing the values of notes & coins
    * equivalent to the change for a given transaction
--- test/test.js
+++ test/test.js
@@ -8,8 +8,8 @@ describe('Cash Register', function(){
       assert.equal(typeof C.getChange, 'function');
     });
 
-    it('getChange(210,300) should equal [50,20,20]', function(){
-        assert.deepEqual(C.getChange(210,300), [50, 20, 20]);
+    it('getChange(220,310) should equal [50,20,20]', function(){
+        assert.deepEqual(C.getChange(220,310), [50, 20, 20]);
     }); // use .deepEqual for arrays see: http://stackoverflow.com/questions/13225274/
 
     it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
