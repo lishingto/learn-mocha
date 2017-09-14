f231ac45675992f80c5ca790cf00503fe0d43ce7
Li Shing To <lishingto@gmail.com>
2017-09-14 15:27:21 +0800

    Conflict attempt 1

--- cash.js
+++ cash.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const C = {
-  coins : [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1],
+  coins : [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1],
   /**
    * getChange returns and Array containing the values of notes & coins
    * equivalent to the change for a given transaction
