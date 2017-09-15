c9caea5a5563ff15745970d669efc4346fa4da91
Li Shing To <lishingto@gmail.com>
2017-09-15 11:44:33 +0800

    Minor bug fixes

--- cash.js
+++ cash.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const C = {
-  coins : [8888, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1],
+  coins : [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1],
 
   /**
    * getChange returns and Array containing the values of notes & coins
