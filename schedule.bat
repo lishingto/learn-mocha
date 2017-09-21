f96eb1813ac8ed76b077692b68e224b8d535c2f8
Li Shing To <shingto.li@rutledgeservices.com>
2017-09-21 11:15:01 +0800

    Daily backup by Windows Batch - Thu 09/21/2017

--- schedule.bat
+++ schedule.bat
@@ -1,1 +1,6 @@
-SCHTASKS /Create /tn "Git push daily LM" /tr D:\Development\unsync\RND\learn-mocha\task.bat /sc daily /st 11:00
+SCHTASKS /Create ^
+/tn "Git push daily LM" ^
+/tr D:\Development\unsync\RND\learn-mocha\task.bat ^
+/sc daily ^
+/st 11:15 ^
+/ru "SYSTEM"
--- task.bat
+++ task.bat
@@ -1,10 +1,12 @@
 D:
 cd D:\Development\unsync\RND\learn-mocha
 
+REM Change user details
+
 git config user.name "Li Shing To"
 git config user.email shingto.li@rutledgeservices.com
 
 git pull
 git add .
-git commit -m "Daily backup by Windows Batch"
+git commit -m "Daily backup by Windows Batch - %DATE%"
 git push
